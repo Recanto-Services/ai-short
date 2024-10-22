f2e31b1c52f6782dd714e93958ba67b2f11d2a5c
Esdras Albino <esdrashenrique475@gmail.com>
2024-10-22 15:31:09 -0300

    first commit

---
+++ Components/Edit.py
@@ -0,0 +1,41 @@
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy.editor import VideoFileClip
+import subprocess
+from Components.FaceCrop import convert_to_vertical, combine_videos
+
+
+def extractAudio(video_path):
+    try:
+        video_clip = VideoFileClip(video_path)
+        audio_path = "audio.wav"
+        video_clip.audio.write_audiofile(audio_path)
+        video_clip.close()
+        print(f"Extracted audio to: {audio_path}")
+        return audio_path
+    except Exception as e:
+        print(f"An error occurred while extracting audio: {e}")
+        return None
+
+
+def crop_videos(input_file, intervals):
+    with VideoFileClip(input_file) as video:
+        for i, (start_time, end_time) in enumerate(intervals):
+            croped = f"croped{i+1}.mp4"
+            output_file = f"output_{i+1}.mp4"
+            cropped_video = video.subclip(start_time, end_time)
+
+            # combine_videos(output_file, croped, "Final.mp4")
+            cropped_video.write_videofile(output_file, codec='libx264')
+            convert_to_vertical(output_file, croped)
+            print(f"Generated {output_file} from {start_time} to {end_time}")
+
+
+# Example usage:
+if __name__ == "__main__":
+    input_file = r"Example.mp4"  # Test
+    print(input_file)
+    output_file = "Short.mp4"
+    start_time = 31.92
+    end_time = 49.2
+
+    crop_video(input_file, output_file, start_time, end_time)
---
+++ main.py
@@ -0,0 +1,37 @@
+from Components.YoutubeDownloader import download_youtube_video
+from Components.Edit import extractAudio, crop_videos
+from Components.Transcription import transcribeAudio
+from Components.LanguageTasks import GetHighlight
+import random
+
+url = input("Enter YouTube video URL: ")
+Vid = download_youtube_video(url)
+if Vid:
+    Vid = Vid.replace(".webm", ".mp4")
+    print(f"Downloaded video and audio files successfully! at {Vid}")
+
+    Audio = extractAudio(Vid)
+    if Audio:
+
+        transcriptions = transcribeAudio(Audio)
+        if len(transcriptions) > 0:
+            TransText = ""
+            print(f"Transcriptions found: {transcriptions}")
+            intervals = []
+            duration = random.randint(30, 55)
+            for text, start, end in transcriptions:
+                if end - start < duration:
+                    end = start + duration
+                intervals.append((start, end))
+
+            # start, stop = GetHighlight(TransText)
+            # print(f"Start: {start} , End: {stop}")
+
+            crop_videos(Vid, intervals)
+
+        else:
+            print("No transcriptions found")
+    else:
+        print("No audio file found")
+else:
+    print("Unable to Download the video")
